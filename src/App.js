bf05fb589bf1c53765c916c85f586340f5797ff7
Janki Modi <jankimodi24@gmail.com>
2024-08-22 09:36:43 +0530

    links added

--- src/App.js
+++ src/App.js
@@ -81,8 +81,8 @@ section {
               <Link to="/about">About</Link>
               <Link to="/directory">Directory</Link>
               <Link to="/diet">Diet</Link>
-
-
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
             </nav>
 
           </header>
